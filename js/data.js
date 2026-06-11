c49347ee3722fc446c437322cf630b4b69297843
amalmanikkath <amalmanikkath@gmail.com>
2026-06-11 16:56:11 +0530

    image name updates

--- js/data.js
+++ js/data.js
@@ -260,7 +260,7 @@ const PRODUCTS = [
       'Ideal for Cooking & Massage',
       'Traditional Nutritional Benefits'
     ],
-    image: 'images/sesamePetBottle.jpeg',
+    image: 'images/sesameOil.jpg',
     inStock: true,
     weight: '1 litre',
     rating: 4.8,
